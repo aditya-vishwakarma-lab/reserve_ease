f653b226d43468e786cfa8e21d0a8229424af1f3
Aditya Kumar Vishwakarma <vishwakarmaaditya03@gmail.com>
2025-08-13 11:47:39 +0530

    feat: add user role

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   has_many :sessions, dependent: :destroy
 
   normalizes :email_address, with: ->(e) { e.strip.downcase }
+  enum role: { customer: 0, staff: 1 }
+
+  STAFF_EMAILS = [
+    'admin@restaurant.com',
+    'manager@restaurant.com'
+  ].freeze
+
+  def self.staff_email?(email)
+    STAFF_EMAILS.include?(email.downcase)
+  end
 end
